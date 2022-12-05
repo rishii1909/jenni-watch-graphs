9a10b9ca6f0c53aaace2763c1ae7ca312a028173
Rishi <hrushikeshchapke@gmail.com>
2022-12-06 02:53:16 +0530

    added filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Divider, Loader, Notification, Text } from "@mantine/core";
+import { Divider, Loader, Notification, RangeSlider, Switch, Text } from "@mantine/core";
 import { SetStateAction, useEffect, useRef, useState } from "react";
 import CSVReader from "react-csv-reader";
 import { ThemeProvider } from "./ThemeProvider";
@@ -6,7 +6,7 @@ import "./index.css"
 import { Area, AreaChart, CartesianGrid, Label, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import Hotkeys from 'react-hot-keys';
 import DataTable from 'react-data-table-component';
-import { capitalize, sum } from "lodash";
+import { capitalize, max, mean, sum } from "lodash";
 import { IconSquareArrowDown } from "@tabler/icons";
 
 const papaparseOptions = {
@@ -19,7 +19,7 @@ const papaparseOptions = {
       .replace(/\W/g, '_')
 }
 
-
+const CLEAN_OUTLIERS_DIFF = 30
 
 
 export default function App() {
@@ -31,17 +31,21 @@ export default function App() {
 
 
   const [data, setData] = useState<any | null>(null)
+  const [customizedData, setCustomizedData] = useState<any | null>(null)
   const [tableData, setTableData] = useState<any>(null)
   const [summationGraph, setSummationGraph] = useState<null | any[]>(null)
   const [videoLoaded, setVideoLoaded] = useState(false)
-  const [pagination, setPagination] = useState(0)
-
   const [columns, setColumns] = useState<any>(null)
 
-  useEffect(() => {
-    if(!data) return
+  const [triedDemo, setTriedDemo] = useState(false)
+  const [meanReplaysCount, setMeanReplaysCount] = useState<[number, number]>([0, 10])
+  const [highestReplaysCount, setHighestReplaysCount] = useState<[number, number]>([0, 100])
+  const [totalWatchDuration, setTotalWatchDuration] = useState<[number, number]>([0, 600])
 
-    let columns = [...Object.keys(data[0]), 'total_watch_duration'].map(key => {
+  useEffect(() => {
+    if (!data) return
+
+    let columns = [...Object.keys(data[0]), 'total_watch_duration', 'highest_replays_count', 'mean_replays_count'].map(key => {
       return {
         name: key.split('_').map(word => capitalize(word)).join(' '),
         key: key,
@@ -53,17 +57,56 @@ export default function App() {
       }
     }).filter(field => !['watchgraph', 'event_name', 'time'].includes(field.key))
 
-    console.log(columns)
     setColumns(columns)
 
     let updateData = data
     updateData = updateData.map((row: any) => {
+
       row.total_watch_duration = sum(row.watchgraph)
+      row.highest_replays_count = max(row.watchgraph)
+      row.mean_replays_count = Math.floor(mean(row.watchgraph) * 100) / 100
       return row
-    })
-    console.log(updateData)
+    }).filter((row: any) => row.highest_replays_count < CLEAN_OUTLIERS_DIFF)
+
+    setCustomizedData(updateData)
     setTableData(updateData)
   }, [data])
+
+  useEffect(() => {
+    if(!customizedData) return
+    let filtered = customizedData
+
+    filtered = filtered.filter((row: any) => row.tried_demo === triedDemo)
+    filtered = filtered.filter((row: any) => row.total_watch_duration >= totalWatchDuration[0] && row.total_watch_duration <= totalWatchDuration[1])
+    filtered = filtered.filter((row: any) => row.highest_replays_count >= highestReplaysCount[0] && row.highest_replays_count <= highestReplaysCount[1])
+
+    setTableData(filtered)
+
+    const summationArray: {
+      sum: number,
+      users: number,
+      position: number
+    }[] = []
+
+    filtered.forEach((row: any) => {
+      row.watchgraph.forEach((val: number, moment: number) => {
+        // Total summation for each sec records.
+        while (moment >= summationArray.length) summationArray.push({
+          sum: 0,
+          users: 0,
+          position: moment
+        })
+
+        summationArray[moment].sum += val
+        summationArray[moment].users += (val != 0) ? 1 : 0
+      });
+    });
+
+    setSummationGraph(summationArray)
+
+    // if (areaRef.current) areaRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+  }, [triedDemo, totalWatchDuration, highestReplaysCount])
+
 
 
   return (
@@ -84,7 +127,7 @@ export default function App() {
                 setData(data)
 
 
-                const summationArray: number[] = []
+                const summationArray: { sum: number, users: number }[] = []
                 // let userScores: {
                 //   name: string | any,
                 //   score: number,
@@ -99,15 +142,21 @@ export default function App() {
                   arr.forEach((reps, position) => {
 
                     // Total summation for each sec records.
-                    while (position >= summationArray.length) summationArray.push(0)
-                    summationArray[position] += reps
+                    while (position >= summationArray.length) summationArray.push({
+                      sum: 0,
+                      users: 0
+                    })
+
+                    summationArray[position].sum += reps
+                    if (reps > 0) summationArray[position].users++
                   })
                 })
 
                 const summationGraph: SetStateAction<any[] | null> = []
 
-                summationArray.forEach((sum: number, position: number) => summationGraph.push({
+                summationArray.forEach(({ sum, users }, position) => summationGraph.push({
                   sum,
+                  users,
                   position
                 }))
 
@@ -129,104 +178,159 @@ export default function App() {
         }
 
         {data &&
-          <div className="flex items-center pl-4" style={{ backgroundColor: "rgba(68, 47, 77, 0.4)", height: "100vh", width: "100vw", overflow: "scroll" }}>
-            <video
-              className={`rounded-xl ${!videoLoaded && 'bg-slate-100 animate-pulse'
-                }`}
-              ref={videoRef}
-              width={540}
-              height={308}
-              preload="auto"
-              onLoadedData={() => setVideoLoaded(true)}
-              src="https://firebasestorage.googleapis.com/v0/b/speare.appspot.com/o/jenni-onboarding-tutorial.mp4?alt=media&token=fbbcff83-e083-4270-8928-1837e6fda57d"
-              controls
-            />
+          <>
+            <div className="flex items-center pl-4" style={{ backgroundColor: "rgba(68, 47, 77, 0.4)", height: "100vh", width: "100vw", overflow: "scroll" }}>
+              <video
+                className={`rounded-xl ${!videoLoaded && 'bg-slate-100 animate-pulse'
+                  }`}
+                ref={videoRef}
+                width={540}
+                height={308}
+                preload="auto"
+                onLoadedData={() => setVideoLoaded(true)}
+                src="https://firebasestorage.googleapis.com/v0/b/speare.appspot.com/o/jenni-onboarding-tutorial.mp4?alt=media&token=fbbcff83-e083-4270-8928-1837e6fda57d"
+                controls
+              />
 
-            <div className="ml-4 p-4 flex bg-slate-100 justify-center items-center" style={{ height: "100%", width: "100%", overflow: "scroll" }}>
-              {!videoLoaded ? <Loader /> :
-                <div
-                  style={{ height: "100%", width: "100%" }}
-                >
+              <div className="ml-4 p-4 flex bg-slate-100 justify-center items-center" style={{ height: "100%", width: "100%", overflow: "scroll" }}>
+                {!videoLoaded ? <Loader /> :
+                  <div
+                    style={{ height: "100%", width: "100%" }}
+                    ref={areaRef}
+                  >
 
-                  {summationGraph &&
-                    <>
-                      <Text size='lg' ref={areaRef}  color='black'>Tutorial viewership</Text>
-                      {/* <Area 
-                      style={{height : "400px"}} 
-                      data={summationGraph}  
-                      {...config} 
-                      onEvent={(chart, event ) => {
-                        // console.log(chart, event)
-                      }}
-                    /> */}
-                      <ResponsiveContainer aspect={2} maxHeight={400}>
-                        <AreaChart
-                          data={summationGraph}
-                          onClick={(nextState, event) => {
-                            const payload = nextState.activePayload ? nextState.activePayload[0].payload : null
-                            if (!payload || !videoRef.current) return
+                    {summationGraph &&
+                      <>
+                        {/* <Text size='lg' ref={areaRef} color='black'>Tutorial viewership</Text> */}
+                        <ResponsiveContainer>
+                          <AreaChart
+                            data={summationGraph}
+                            onClick={(nextState, event) => {
+                              const payload = nextState.activePayload ? nextState.activePayload[0].payload : null
+                              if (!payload || !videoRef.current) return
+                              videoRef.current.currentTime = payload.position
+                            }}
+                          >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="position" dy={16}>
 
-                            videoRef.current.currentTime = payload.position
-                          }}
-                        >
-                          <CartesianGrid strokeDasharray="3 3" />
-                          <XAxis dataKey="position" dy={16}>
-                            <Label
-                              value="Position in secs"
+                            </XAxis>
+                            <YAxis dataKey="sum">
+                              <Label
+                                value="Views"
+                                position="insideLeft"
+                                angle={-90}
+                                style={{ textAnchor: "middle" }}
+                              />
+                            </YAxis>
+                            <YAxis dataKey="users">
+                              <Label
+                                value="Views"
+                                position="insideLeft"
+                                angle={-90}
+                                style={{ textAnchor: "middle" }}
+                              />
+                            </YAxis>
+                            <Tooltip
+                              label="views"
+                              formatter={(value, name) => `${value} ${name === "users" ? "unique users" : "total views"}`}
+                              labelFormatter={(value, name) => `At ${value} seconds`}
                             />
-                          </XAxis>
-                          <YAxis dataKey="sum">
-                            <Label
-                              value="Views"
-                              position="insideLeft"
-                              angle={-90}
-                              style={{ textAnchor: "middle" }}
-                            />
-                          </YAxis>
-                          <Tooltip
-                            label="views"
-                            formatter={(value, name, props) => `${value} views`}
-                            labelFormatter={(value, name) => `At ${value} seconds`}
-                          />
-                          <Area type="monotone" dataKey="sum" stroke="#8884d8" fill="#8884d8" />
-                        </AreaChart>
-                      </ResponsiveContainer>
-                      <br />
-                      <Divider color="gray" />
+                            <Area type="monotone" dataKey="users" stroke="#2C514C" fill="#2C514C" />
+                            <Area type="monotone" dataKey="sum" stroke="#8884d8" fill="#8884d8" />
+                          </AreaChart>
+                        </ResponsiveContainer>
+                        <br />
+                        <Divider color="gray" />
+                      </>
+                    }
 
-
-
-
-                      {/* Data Table */}
-                      <DataTable
-                        columns={columns}
-                        data={tableData}  
-                        sortIcon={<IconSquareArrowDown/>}
-                        pagination
-                        onRowClicked={(row: {
-                          watchgraph : Array<number>
-                        }) => {
-                          const graphArray = new Array
-                          row.watchgraph.forEach((value, position) => {
-                            graphArray.push({
-                              sum: value,
-                              position
-                            })
-                          });
-                          if(areaRef.current) areaRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
-                          console.log("graphArray for current user", graphArray)
-                          setSummationGraph(graphArray)
-                        }}
-
-                        pointerOnHover
-                        highlightOnHover
-                      />
-                    </>
-                  }
-                </div>
-              }
+                  </div>
+                }
+              </div>
             </div>
-          </div>
+            {(columns && tableData) &&
+              <div className=" space-y-4 text-black">
+                {/* Data Table */}
+                <div className="px-2 space-y-3">
+                  <Switch
+                    label="Used Picture in Picture"
+                    checked={triedDemo}
+                    onChange={() => {
+                      const status = !triedDemo
+                      setTriedDemo(status)
+
+                    }}
+                  />
+                  <br />
+                  <div>
+                    <Text color="white">Total watch duration : {totalWatchDuration[0]} - {totalWatchDuration[1]} seconds</Text>
+                    <RangeSlider
+                      label="Total watch duration"
+                      min={0}
+                      max={600}
+                      value={totalWatchDuration}
+                      onChange={(val) => {
+                        setTotalWatchDuration(val)
+
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <Text color="white">Highest Replays count : Replayed {highestReplaysCount[0]} - {highestReplaysCount[1]} times</Text>
+                    <RangeSlider
+                      label="Highest Replays count"
+                      min={0}
+                      max={100}
+                      value={highestReplaysCount}
+                      onChange={(val) => {
+                        console.log(customizedData)
+                        setHighestReplaysCount(val)
+
+                      }}
+                    />
+                  </div>
+                  {/* <div>
+                  <Text color="white">Mean Replays count : Replayed {meanReplaysCount[0]} - {meanReplaysCount[1]} times </Text>
+                  <RangeSlider
+                    label="Mean Replays count"
+                    min={0}
+                    max={10}
+                    value={meanReplaysCount}
+                    onChange={(val) => {
+                      console.log(meanReplaysCount)
+                      setMeanReplaysCount(val)
+                      setTableData(customizedData.filter((row: any) => row.mean_replays_count >= val[0] && row.mean_replays_count <= val[1]))
+                    }}
+                  />
+                </div> */}
+                </div>
+                <DataTable
+                  columns={columns}
+                  data={tableData}
+                  sortIcon={<IconSquareArrowDown />}
+                  pagination
+                  onRowClicked={(row: {
+                    watchgraph: Array<number>
+                  }) => {
+                    const graphArray = new Array
+                    row.watchgraph.forEach((value, position) => {
+                      graphArray.push({
+                        sum: value,
+                        position
+                      })
+                    });
+                    if (areaRef.current) areaRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+                    console.log("graphArray for current user", graphArray)
+                    setSummationGraph(graphArray)
+                  }}
+
+                  pointerOnHover
+                  highlightOnHover
+                />
+              </div>
+            }
+          </>
         }
 
       </ThemeProvider>
